da270ab92076083a1ffa17d2e19d423cdd91602c
Pedro Virgilio <pvvirg16@gmail.com>
2022-10-06 20:58:56 -0300

    Cardápio de refeicões/degustações/bebidas

--- trabalho01.cpp
+++ trabalho01.cpp
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <locale.h>
 
+
 //Estrutura do Produto
 struct produto{
 	char nome[50];
@@ -10,37 +11,78 @@ struct produto{
 	int qtd;
 };
 
-//Função para criação do Pedido
+//Funï¿½ï¿½o para criaï¿½ï¿½o do Pedido
 int fpedido(){
 	int option;
 	printf("------------ CARDAPIO --------------\n");
-	printf("| 1 - Refeições | 2 - Degustações  |\n");
+	printf("| 1 - RefeiÃ§Ãµes | 2 - DegustaÃ§Ãµes  |\n");
 	printf("| 3 - Bebidas   |                  |\n");
 	printf("------------------------------------\n\n");
-	printf("Escolha a opção que você deseja fazer o pedido: ");
+	printf("Escolha a opÃ§Ã£oo que vocÃŠ deseja fazer o pedido: ");
 	scanf("%d", &option);
+	switch (option){
+	case 1: {
+		printf("---------- CARDAPIO ----------\n");
+		printf("| 1 - Bife com batata frita   |\n");
+		printf("-------------------------------\n");
+		printf("| 2 - Lasanha                 |\n");
+		printf("-------------------------------\n");
+		printf("| 3 - Maria Isabel com frango |\n");
+		printf("-------------------------------\n");
+		printf("| 4 - Panelada                |\n");
+		printf("-------------------------------");
+		break;
+	}
+	case 2:{
+		printf("---------- CARDAPIO ----------\n");
+		printf("| 1 - Batata Frita          |\n");
+		printf("-----------------------------\n");
+		printf("| 2 - Enrolados de Carne    |\n");
+		printf("-----------------------------\n");
+		printf("| 3 - Enrolados de Presunto |\n");
+		printf("-----------------------------\n");
+		printf("| 4 - Petisco Sacanagem     |\n");
+		printf("-----------------------------");
+		break;
+	}
+	case 3:{
+		printf("---------- CARDAPIO ----------\n");
+		printf("| 1 - Coca-Cola                  |\n");
+		printf("-----------------------------\n");
+		printf("| 2 - Suco (MaracujÃ¡ e Acerola)  |\n");
+		printf("-----------------------------\n");
+		printf("| 3 - Cerveja (Skol, Brahma)     |\n");
+		printf("-----------------------------\n");
+		printf("| 4 - Ãgua com GÃ¡s               |\n");
+		printf("-----------------------------");
+		break;
+
+	}
+		
+	}
+	
 	return 0;
 }
 
-//Função de criação de alteração do pedido
+//Funï¿½ï¿½o de criaï¿½ï¿½o de alteraï¿½ï¿½o do pedido
 int alterpedido(){
 	printf("Criar menu de alterar pedido");
 }
 
-//Função de criação de consultar pedido
+//Funï¿½ï¿½o de criaï¿½ï¿½o de consultar pedido
 int consulpedido(){
 	printf("Criar menu de cunsultar pedido");
 }
 
 //Menu Iniciar do Aplicativo
 int main(){
-	int choice;
 	setlocale(LC_ALL, "Portuguese");
+	int choice;
 	printf("-----------------------------------------------\n");
 	printf("| 1 - Fazer Pedido     | 2 - Alterar Pedido |\n");
 	printf("| 3 - Consultar Pedido | 4 - Sair           |\n");
 	printf("------------------------------------------\n");
-	printf("Escolha uma opção: ");
+	printf("Escolha uma opÃ§Ã£o: ");
 	scanf("%d", &choice);
 	switch(choice){
 		case 1: {
@@ -60,7 +102,7 @@ int main(){
 		}
 		case 4: {
 			system("cls");
-			printf("Saindo da aplicação...");
+			printf("Saindo da aplicaÃ§Ã£o...");
 			return 0;
 			break;
 		}
